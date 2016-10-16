04256e0501f9d60e84c483f19f15880e8ee2d801
Annia Martinez <Annia Martinez>
2016-10-16 17:10:04 -0400

    img

--- boondoggle_Assignment/_content/_js/app/app.js
+++ boondoggle_Assignment/_content/_js/app/app.js
@@ -1,16 +1,16 @@
 angular.module('myApp', []).controller('appCtrl', function($scope) {
     $scope.ambities = [
-        { title: '# Ambitie05', subtitle1: 'Ondernemen', subtitle2: 'Mobilitiet', text: 'Een manier vinden om het gebruik van smartphones tijdens het rijden terug te dringen.' },
-        { title: '# Ambitie06', subtitle1: 'Mobilitiet', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.' },
-        { title: '# Ambitie07', subtitle1: 'Mobilitiet', subtitle2: '', text: 'We willen carsharing nieuw leven inblazen.' },
-        { title: '# Ambitie08', subtitle1: 'Ondernemen', subtitle2: '', text: 'We willen carsharing nieuw leven inblazen.' },
-        { title: '# Ambitie09', subtitle1: 'Mobilitiet', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.' },
-        { title: '# Ambitie10', subtitle1: 'Ondernemen', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.' },
+        { title: '# Ambitie05', subtitle1: 'Ondernemen', subtitle2: 'Mobilitiet', text: 'Een manier vinden om het gebruik van smartphones tijdens het rijden terug te dringen.', img: "_content/_images/blocks1.png" },
+        { title: '# Ambitie06', subtitle1: 'Mobilitiet', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.', img: "_content/_images/blocks2.png" },
+        { title: '# Ambitie07', subtitle1: 'Mobilitiet', subtitle2: '', text: 'We willen carsharing nieuw leven inblazen.', img: "_content/_images/blocks3.png" },
+        { title: '# Ambitie08', subtitle1: 'Ondernemen', subtitle2: '', text: 'We willen carsharing nieuw leven inblazen.', img: "_content/_images/blocks1.png" },
+        { title: '# Ambitie09', subtitle1: 'Mobilitiet', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.', img: "_content/_images/blocks2.png" },
+        { title: '# Ambitie10', subtitle1: 'Ondernemen', subtitle2: 'Wonen', text: 'We willen het aantal ongevallen rondom scholen halveren.', img: "_content/_images/blocks3.png" },
 
     ];
 
     $scope.acties = [
-        { number: '1', class: 'ambie07', title: '# Ambitie07', subtitle1: 'Mobilitiet', subtitle2: '', text: 'Heel Nederland in Automodus.', textSmall: '', textMedium: 'Campagne:' },
+        { number: '1', class: 'ambie07', title: '# Ambitie07', subtitle1: '', subtitle2: 'Mobilitiet', text: 'Heel Nederland in Automodus.', textSmall: '', textMedium: 'Campagne:' },
         { number: '2', class: 'ambie05', title: '# Ambitie05', subtitle1: 'Mobilitiet', subtitle2: 'Wonen', text: 'Nieuwe onderzoeks-resultaten rond domotica vs inbraak..', textSmall: '', textMedium: '' },
         { number: '3', class: 'ambie04', title: '# Ambitie04', subtitle1: 'Mobilitiet', subtitle2: 'Ondernemen', text: 'Freelancers + veilige wifi, Webinar di 14 juni 2016', textSmall: '', textMedium: '' },
         { number: '4', class: 'ambie06', title: '# Ambitie07', subtitle1: 'Ondernemen', subtitle2: '', text: 'Ongevallen met driverless cars.', textSmall: 'Livestrem', textMedium: 'Debat TU Delft:' },
